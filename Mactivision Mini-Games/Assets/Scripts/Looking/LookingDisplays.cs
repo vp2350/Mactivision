03f1c9745b4dc2c30f5c6c78b1ca6b37ba2d6445
Vansh Pahuja <vp2350@rit.edu>
2023-03-07 13:53:29 -0500

    LookingLevelManager functions added

---
+++ Mactivision Mini-Games/Assets/Scripts/Looking/LookingDisplays.cs
@@ -0,0 +1,169 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+// This class is responsible for managing the games foods, and dispensing of foods.
+public class LookingDisplays : MonoBehaviour
+{
+    public Animator pipe;               // this class will play the pipe dispensing animation
+    public Transform monitor;           // the monitor that shows food updates
+    public GameObject screenGreen;      // monitor flashes green when food is updated to preferred
+    public GameObject screenRed;        // monitor flashes red when food is updated to unpreferred
+    public GameObject[] allFoods;       // array of all possible foods
+    public AudioClip display_sound;    // sound played when a food is dispensed from the pipe
+    public AudioClip prompt_sound;      // sound played when there is a food update
+    AudioSource sound;
+
+    System.Random randomSeed;   // seed of the current game
+    float avgUpdateFreq;        // average number of foods dispensed between each food update
+    float updateFreqVariance;   // variance of `avgUpdateFreq`
+    int lastUpdate = 0;         // number of foods dispensed since last food update
+    int nextUpdate = 0;
+
+    string[] gameFoods;                                 // foods being used in the current game
+    public string goodFood { private set; get; }     // foods the monster will eat
+    string[] badFoods;                                  // foods the monster will spit out
+    int goodFoodCount = 0;                              // number of foods the monster likes
+
+    GameObject screenFood;                                  // the food shown on the screen during a food update
+    public string currentFood { private set; get; }         // the current food the player has to decide on
+    public DateTime choiceStartTime { private set; get; }   // the time the current food is dispensed and the player can make a choice
+
+    // Initializes the dispenser with the seed.
+    // Randomly chooses which foods will be used in the game.
+    // `gameFoods` has the list of `tf` food items that will
+    // be used in the game, and initially, `badFoods` = `gameFoods`,
+    // as all foods begin "unpreferred". `goodFoods` is an array of empty
+    // strings the same length as `gameFoods` and `badFoods`.
+    public void Init(string seed, int tf, float uf, float sd)
+    {
+        screenGreen.SetActive(false);
+        screenRed.SetActive(false);
+        foreach (GameObject obj in allFoods) obj.SetActive(false);
+
+        sound = gameObject.GetComponent<AudioSource>();
+
+        randomSeed = new System.Random(seed.GetHashCode());
+        avgUpdateFreq = uf;
+        updateFreqVariance = sd;
+
+        gameFoods = new string[tf];
+
+
+        goodFood = allFoods[randomSeed.Next(allFoods.Length)].name;
+        
+    }
+
+    // Decides whether to update the list of liked foods.
+    // Returns whether there is an update or not
+    public bool DispenseNext()
+    {
+        bool update = false;
+
+        if (++lastUpdate >= nextUpdate || goodFoodCount == 0)
+        {
+            UpdateFoods();
+            update = true;
+            lastUpdate = 0;
+            nextUpdate = FoodsBetweenNextUpdate(avgUpdateFreq, updateFreqVariance);
+            StartCoroutine(WaitForFoodUpdate(1.75f)); // wait for a food update and then dispense next
+        }
+        else
+        {
+            StartCoroutine(WaitForFoodUpdate(0f)); // instantly dispense next food
+        }
+
+        return update;
+    }
+
+    //Returns whether the input matches correct item
+    public bool MakeChoice(string[] inputList, int inputNumber)
+    {
+        return (inputList[inputNumber] == goodFood) ? true : false;
+    }
+
+    // The actual function that randomly chooses a food and dispenses it.
+    // Sets the `choiceStartTime` to the current time and activate the 
+    // food GameObject and places it "in the pipe".
+    // Physics does the rest to make it fall out of the pipe.
+    void Dispense()
+    {
+        int randIdx;
+        randIdx = randomSeed.Next(gameFoods.Length);
+        currentFood = gameFoods[randIdx];
+        choiceStartTime = DateTime.Now;
+
+        // find the current food GameObject and place it in the pipe
+        foreach (GameObject obj in allFoods)
+        {
+            if (obj.name == currentFood)
+            {
+                obj.SetActive(true);
+                obj.transform.position = new Vector3(-1f, 4f, 0f);
+                break;
+            }
+        }
+
+        // dispensing animation and sound
+        pipe.Play("Base Layer.pipe_dispense");
+        sound.clip = dispense_sound;
+        sound.PlayDelayed(0f);
+    }
+
+    // Update the list of good and bad foods. Essentially swaps items between
+    // the good and bad lists. If there are less than two foods in the good list, 
+    // it will move a food from the bad list to the good list. 
+    // On update, this function will activate the flashing green or red screen
+    // depending on if a food was moved to the good or bad list, respectively.
+    void UpdateFoods()
+    {
+        // choose a food to update and swap it to the other list
+        int randIdx = randomSeed.Next(gameFoods.Length);
+        string food = badFoods[randIdx];
+        if (goodFoodCount < 2)
+        {
+            do
+            {
+                randIdx = randomSeed.Next(gameFoods.Length);
+                food = badFoods[randIdx];
+            } while (food == "");
+        }
+        badFoods[randIdx] = goodFoods[randIdx];
+        goodFoods[randIdx] = food;
+
+        // show the food to be updated on the monitor, and activate either
+        // the green or red flashing screen animation and sound
+        if (food == "")
+        {
+            food = badFoods[randIdx];
+            screenRed.SetActive(true);
+            goodFoodCount--;
+        }
+        else
+        {
+            screenGreen.SetActive(true);
+            goodFoodCount++;
+        }
+        food = food + " (screen)";
+        screenFood = monitor.Find(food).gameObject;
+        screenFood.SetActive(true);
+        sound.PlayOneShot(screen_sound);
+    }
+
+    // Wait for the flashing screen animation and then dispense the next food.
+    IEnumerator WaitForFoodUpdate(float wait)
+    {
+        yield return new WaitForSeconds(wait);
+        screenGreen.SetActive(false);
+        screenRed.SetActive(false);
+        screenFood.SetActive(false);
+        Dispense();
+    }
+
+    int FoodsBetweenNextUpdate(float avg, float sd)
+    {
+        float rand = (float)randomSeed.NextDouble();
+        return (int)Mathf.Round((sd + 0.1333f) * 30f * Mathf.Pow(rand - 0.5f, 3f) + avg);
+    }
+}
--- Mactivision Mini-Games/Assets/Scripts/Looking/LookingLevelManager.cs
+++ Mactivision Mini-Games/Assets/Scripts/Looking/LookingLevelManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -64,6 +65,42 @@ public class LookingLevelManager : LevelManager
         //right = 1
         //down = 2
         //left = 3
+        if (lvlState == 2)
+        {
+            StartGame();
+
+            // game automatically ends after maxGameTime seconds
+            if (Time.time - gameStartTime >= maxGameTime)
+            {
+                EndGame();
+                return;
+            }
+
+            // The game cycle
+            switch (gameState)
+            {
+                case GameState.Prompting:
+                    break;
+                case GameState.DisplayOptions:
+                    DisplayOptions();
+                    break;
+                case GameState.WaitingForPlayer:
+                    WaitForPlayer();
+                    break;
+                case GameState.Response:
+                    GiveFeedback();
+                    break;
+            }
+        }
+    }
+
+    // Begin the actual game, start recording metrics
+    void StartGame()
+    {
+        //mcMetric.startRecording();
+        //metricWriter = new MetricJSONWriter("Feeder", DateTime.Now, seed); // initialize metric data writer
+        gameStartTime = Time.time;
+        //sound.clip = bite_sound;
     }
     // Handles GUI events (keyboard, mouse, etc events)
     void OnGUI()
@@ -87,5 +124,48 @@ public class LookingLevelManager : LevelManager
         {
             Battery.Instance.LoadNextScene();
         }
+    }
+
+    void DisplayOptions()
+    {
+
+    }
+
+    void WaitForPlayer()
+    {
+
+    }
+
+    void GiveFeedback()
+    {
+
+    }
+
+    // End game, stop animations, sounds, physics. Finish recording metrics
+    void EndGame()
+    {
+        //mcMetric.finishRecording();
+        //var str = metricWriter.GetLogMetrics(
+        //            DateTime.Now,
+        //            new List<AbstractMetric>() { mcMetric }
+        //        );
+        //StartCoroutine(Post("feeder_" + DateTime.Now.ToFileTime() + ".json", str));
+        //
+        //dispenser.StopAllCoroutines();
+        //dispenser.screenRed.SetActive(false);
+        //dispenser.screenGreen.SetActive(false);
+        //dispenser.enabled = false;
+        //monster.speed = 0f;
+        //foreach (GameObject obj in GameObject.FindGameObjectsWithTag("Food"))
+        //{
+        //    obj.GetComponent<Rigidbody2D>().isKinematic = true;
+        //    obj.GetComponent<Rigidbody2D>().velocity = Vector2.zero;
+        //    obj.GetComponent<Rigidbody2D>().angularVelocity = 0f;
+        //}
+        foreach (AudioSource aud in FindObjectsOfType(typeof(AudioSource)) as AudioSource[])
+        {
+            aud.Stop();
+        }
+        EndLevel(0f);
     }
 }
